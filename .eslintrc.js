ecfd54bc2f79f3631929fb2b8cc0fdf902bb42f5
kyungyeon-leekyungyeon-leekyungyeon-leekyungyeon-leekyungyeon-leekyungyeon-leeruddus <ruddus716@gmail.com>
2023-08-01 21:23:24 -0700

    deploy error

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,6 @@ module.exports = {
   rules: {
     'prettier/prettier': 'off',
     'react/react-in-jsx-scope': 'off',
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
     'react/prop-types': 0,
     'no-unused-vars': 0,
     'react/no-unescaped-entities': 0,
